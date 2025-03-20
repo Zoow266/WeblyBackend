9b6e10cb84f6ed23efb945c9a24cf26f6d85f9f3
Vladimir Oleynikov <vovachka321@gmail.com>
2025-03-20 22:32:48 +0300

    DataBaseRender

--- obj/Debug/net9.0/WeblyTest2.AssemblyInfo.cs
+++ obj/Debug/net9.0/WeblyTest2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeblyTest2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f46891065bf9864f220fe727d5060405e57976a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f056a3d5a4455ddc5f2a65e9b3e2767745021b2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeblyTest2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeblyTest2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
